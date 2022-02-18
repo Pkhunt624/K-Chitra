903bc6d021d70ed88ed4ef5a5f99c94aee8a9775
K Chitra pradeep <74489293+Pkhunt624@users.noreply.github.com>
2022-02-18 17:05:56 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Coding',
+      price: 1400,
+      image: 'https://www.computersciencedegreehub.com/wp-content/uploads/2016/02/what-is-coding-1024x683.jpg',
+    },
+    {
+      id: '2',
+      name: 'Hacking',
+      price: 2400,
+      image: 'https://imageio.forbes.com/specials-images/imageserve/617853670/Drive-by-hacker/960x0.jpg?fit=bounds&format=jpg&width=960',
+    },
+    {
+      id: '3',
+      name: 'Web devlopment',
+      price: 1000,
+      image: 'https://www.elegantthemes.com/blog/wp-content/uploads/2018/12/top11.png',
+    },
+  ],
+};
+export default data;
